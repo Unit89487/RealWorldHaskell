fd1302facf1adb818f4620df9ecdea67f9553b59
Jay Abdallah <jay.h.abdallah@gmail.com>
2015-01-27 15:29:37 -0500

    Chapter one type exercises in GHCI

--- ch01.hs
+++ ch01.hs
@@ -8,5 +8,21 @@ main = interact wordCount
 
 
 --Exercises
---
-
+--Check the type of a few statements in GHCI
+--The type of the expression 3 + 4 is :: Num a => a
+--The type of expression 3 + 4.0 is Fractional a => a
+--The type of the expression 3.0 + 3.0 is Fractional a => a
+--The type of the expression 3 * 5 + 3 is Num a => a, again with that double colon :: preceding Num
+--The type of the expression 3 + 4 is Num a => a 
+--The type of the expression (+) 2 3 is Num a => a  
+--The type of the expression is 
+--The type of the expression is 
+--The type of the expression is 
+--The type of the expression is 
+--The type of the expression is 
+--The type of the expression is 
+--The type of the expression is 
+--The type of the expression is 
+--The type of the expression is 
+--The type of the expression is 
+--The type of the expression is 
