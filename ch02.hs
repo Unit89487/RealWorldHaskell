fc8b5074cbe7810ba881701c935ce4fc4a2a3231
Jay Abdallah <jay.h.abdallah@gmail.com>
2015-01-29 06:40:48 -0500

    Started chap 2, chap 1 review.

---
+++ ch02.hs
@@ -0,0 +1,12 @@
+--"There are three interesting aspects to types in Haskell: they are strong, they are static, and they can be automatically inferred."
+--Strong vs weak refers to how permissive a type system is. 
+
+
+--Ch01 Review. Type exercises
+--The type of the expression odd 7 is Bool
+--The type of the expression compare 3 30 is Ordering 
+--In other words, :type compare 3 30 is Ordering 
+--The type of the expression sqrt(81) is Floating a => a  
+--head [1, 2, 3, 4] has type Num a => a
+--head ["chimi, "changa", "Oka", "vanga"] has type [Char]
+--head ['a','b','c','d'] has type Char 
