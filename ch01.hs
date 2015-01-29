d8e842d619eeac55df0f9464a332f6279bf6c249
Jay Abdallah <jay.h.abdallah@gmail.com>
2015-01-28 22:31:32 -0500

    Chap 1 type exercises in ghci

--- ch01.hs
+++ ch01.hs
@@ -15,14 +15,13 @@ main = interact wordCount
 --The type of the expression 3 * 5 + 3 is Num a => a, again with that double colon :: preceding Num
 --The type of the expression 3 + 4 is Num a => a 
 --The type of the expression (+) 2 3 is Num a => a  
---The type of the expression is 
---The type of the expression is 
---The type of the expression is 
---The type of the expression is 
---The type of the expression is 
---The type of the expression is 
---The type of the expression is 
---The type of the expression is 
---The type of the expression is 
---The type of the expression is 
---The type of the expression is 
+--The type of the expression sqrt(16) is Floating a =? a 
+--The type of the expression succ 6 is (Num a, Enum a) => a 
+--The type of the expression succ 7 is (Num a, Enum a) => a 
+--The type of the expression pred 9 is (Num a, Enum a) => a 
+--The type of the expression sin (pi / 2) is Floating a => a  
+--The type of the expression truncate pi is Integral b => b 
+--The type of the expression round 3.5 is Integral b => b
+--The type of the expression round 3.4 is Integral b => b
+--The type of the expression floor 3.7 is Integral b => b
+--The type of the expression ceiling 3.3 is Integral b => b
