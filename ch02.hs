d8ebb2e8cc1ba3e6bd9c29cebdc720b6eede6b15
Jay Abdallah <jay.h.abdallah@gmail.com>
2015-01-29 22:35:22 -0500

    Just keepin the dream alive, chap 2

--- ch02.hs
+++ ch02.hs
@@ -1,3 +1,5 @@
+--Types and Functions 
+
 --"There are three interesting aspects to types in Haskell: they are strong, they are static, and they can be automatically inferred."
 --Strong vs weak refers to how permissive a type system is. 
 
@@ -10,3 +12,12 @@
 --head [1, 2, 3, 4] has type Num a => a
 --head ["chimi, "changa", "Oka", "vanga"] has type [Char]
 --head ['a','b','c','d'] has type Char 
+
+
+numbers = [1, 2, 3, 4, 5]
+
+tim = ("Timbuck", 2)
+bill = ("Beau", 4)
+kim = ("ShuckAnd", 5)
+
+
